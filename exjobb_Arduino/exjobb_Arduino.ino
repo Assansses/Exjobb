36dd89d994f45ebb50105c5b8d8f66cdfa5bd8e4
Assansses <assarjohansson2001@gmail.com>
2025-04-14 14:38:08 +0200

    Update exjobb_Arduino.ino
    Added ED pin

--- exjobb_Arduino/exjobb_Arduino.ino
+++ exjobb_Arduino/exjobb_Arduino.ino
@@ -1,27 +1,49 @@
 /*********************************************************************************************
-* Assar Johansson, 2025-02-17
+* Assar Johansson, 2025-04-14
 *
 * This is a demo to read and wright to the NTP5332 via I2C with the wire library (https://docs.arduino.cc/language-reference/en/functions/communication/wire/)
 * Page and figure numbers refer to the NTP5332 datasheet (https://www.nxp.com/docs/en/data-sheet/NTP53x2.pdf)
 *
-* Figure 76 shows how to calculate the value of the pull-up resistor used on the SCL and SDL
 *
 ***********************************************************************************************/
 #include <Wire.h>
 
 // Device adressing described in 8.3.1.1.5 on page 100
 #define NTP_ADDRESS 0x54 // The Wire library adds W/R-bit in its functions
+#define ED_PIN 5
+
+
+
+void ED_ISR(){
+  Serial.println("ED_ISR has been fired");
+  while(!digitalRead(ED_PIN)){} // While RF-filed is presented
+  Serial.println("Exiting ED_ISR");
+}
+
 
 
 void setup() {
   Serial.begin(9600); // Start serial interface 
 
   Wire.begin(); // join I2C bus (address optional for master)
+
+  pinMode(ED_PIN, INPUT); // Low when RF-filed is presented 
+  
+
 }
 
 
 void loop() {
+  
+ //"Software interrupt"
+  if(!digitalRead(ED_PIN)){ // If filed is presented
+    ED_ISR();
+  } 
 
+
+
+}
+/*
   Serial.println("Writing data (AA) to adress 00 02");
   
   //WRITE DATA p. 102
@@ -32,10 +54,10 @@ void loop() {
   
   // Sends data to be stored
   // 4 Data blocks should be sent for writing to EEPROM
-  Wire.write(0xAA); //DATA 0
-  Wire.write(0xAA); //DATA 1
-  Wire.write(0xAA); //DATA 2
-  Wire.write(0xAA); //DATA 3 
+  Wire.write(0xAB); //DATA 0
+  Wire.write(0xBA); //DATA 1
+  Wire.write(0xFA); //DATA 2
+  Wire.write(0xBA); //DATA 3 
 
   Wire.endTransmission();    // stop transmitting
 
@@ -43,7 +65,7 @@ void loop() {
 
 
   Serial.println("Reading data from adress 00 02");
-  //READ DATA
+  //READ DATA p. 102
   //Prepare adress
   Wire.beginTransmission(NTP_ADDRESS); // Write start adress 
 
@@ -58,6 +80,7 @@ void loop() {
     char c = Wire.read();   // Read bytes as char
     Serial.println(c);      // Print the character
   }
-Serial.println("Done");
-  delay(50000);
+  Serial.println("Done");
+
+*/
 }
