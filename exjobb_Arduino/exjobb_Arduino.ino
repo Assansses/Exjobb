75f8fe739aa8177d6c40bb9563bf2943fca4b0f2
Assansses <assarjohansson2001@gmail.com>
2025-04-14 13:45:53 +0200

    Create exjobb_Arduino.ino
    Initial commit with I2C interface

---
+++ exjobb_Arduino/exjobb_Arduino.ino
@@ -0,0 +1,63 @@
+/*********************************************************************************************
+* Assar Johansson, 2025-02-17
+*
+* This is a demo to read and wright to the NTP5332 via I2C with the wire library (https://docs.arduino.cc/language-reference/en/functions/communication/wire/)
+* Page and figure numbers refer to the NTP5332 datasheet (https://www.nxp.com/docs/en/data-sheet/NTP53x2.pdf)
+*
+* Figure 76 shows how to calculate the value of the pull-up resistor used on the SCL and SDL
+*
+***********************************************************************************************/
+#include <Wire.h>
+
+// Device adressing described in 8.3.1.1.5 on page 100
+#define NTP_ADDRESS 0x54 // The Wire library adds W/R-bit in its functions
+
+
+void setup() {
+  Serial.begin(9600); // Start serial interface 
+
+  Wire.begin(); // join I2C bus (address optional for master)
+}
+
+
+void loop() {
+
+  Serial.println("Writing data (AA) to adress 00 02");
+  
+  //WRITE DATA p. 102
+  Wire.beginTransmission(NTP_ADDRESS); 
+  
+  Wire.write(0x00); // sends first (most significant) bytes of adress
+  Wire.write(0x02); // sends second (least significant) bytes of adress
+  
+  // Sends data to be stored
+  // 4 Data blocks should be sent for writing to EEPROM
+  Wire.write(0xAA); //DATA 0
+  Wire.write(0xAA); //DATA 1
+  Wire.write(0xAA); //DATA 2
+  Wire.write(0xAA); //DATA 3 
+
+  Wire.endTransmission();    // stop transmitting
+
+  delay(500);
+
+
+  Serial.println("Reading data from adress 00 02");
+  //READ DATA
+  //Prepare adress
+  Wire.beginTransmission(NTP_ADDRESS); // Write start adress 
+
+  Wire.write(0x00); // sends first (most significant) bytes of adress
+  Wire.write(0x02); // sends second (least significant) bytes of adress
+
+  Wire.endTransmission(); // Send device adress, memmory adress and a stop bit
+
+  Wire.requestFrom(NTP_ADDRESS, 16);    // Wait for 16 bytes from the device (NTP5332)
+
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read();   // Read bytes as char
+    Serial.println(c);      // Print the character
+  }
+Serial.println("Done");
+  delay(50000);
+}
