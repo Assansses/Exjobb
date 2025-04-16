e99ab2586bb7a0669cbb6d2aa31f18b01f208e41
Assansses <assarjohansson2001@gmail.com>
2025-04-16 11:34:55 +0200

    Update exjobb_Arduino.ino
    Read and write functions for EEPROM

--- exjobb_Arduino/exjobb_Arduino.ino
+++ exjobb_Arduino/exjobb_Arduino.ino
@@ -21,16 +21,12 @@ void ED_ISR(){
 }
 
 
-void NTP_write(byte SL_AD=NTP_ADDRESS, byte BL_AD1=0x00, byte BL_AD0=0x00, int N=1, ...){
+void NTP_EEPROM_write(byte SL_AD, byte BL_AD1, byte BL_AD0, byte DATA0, byte DATA1, byte DATA2, byte DATA3){
+  Serial.print("Writing "); 
+  Serial.print(DATA0, HEX); Serial.print(", "); Serial.print(DATA1, HEX); Serial.print(", "); Serial.print(DATA2, HEX); Serial.print(", "); Serial.print(DATA3, HEX);
+  Serial.print(" to address ");
+  Serial.print(BL_AD1); Serial.println(BL_AD0);
 
-  int l_ParamVal = 0x00;
-
-  va_list l_Arg;
-  va_start(l_Arg, N);
-
-
-
-  Serial.println("Writing data (AA) to adress 00 02");
   
   //WRITE DATA p. 102
   Wire.beginTransmission(SL_AD); 
@@ -39,14 +35,37 @@ void NTP_write(byte SL_AD=NTP_ADDRESS, byte BL_AD1=0x00, byte BL_AD0=0x00, int N
   Wire.write(BL_AD0); // sends second (least significant) bytes of adress
   
   // Sends data to be stored
-  for ( int i; i < N; ++i) {
-    Wire.write(va_arg(l_Arg, byte)); 
+  // 4 Data blocks should be sent for writing to EEPROM
+  Wire.write(DATA0); //DATA 0
+  Wire.write(DATA1); //DATA 1
+  Wire.write(DATA2); //DATA 2
+  Wire.write(DATA3); //DATA 3 
 
-  }
   Wire.endTransmission();    // stop transmitting
 
-  va_end(l_Arg);
+}
+void NTP_EEPROM_read(uint8_t SL_AD, uint8_t BL_AD1, uint8_t BL_AD0){
 
+  Serial.print("Reading data from adress ");
+  Serial.print(BL_AD1); Serial.println(BL_AD0);
+
+  //READ DATA p. 102
+  //Prepare adress
+  Wire.beginTransmission(SL_AD); // Write start adress 
+
+  Wire.write(BL_AD1); // sends first (most significant) bytes of adress
+  Wire.write(BL_AD0); // sends second (least significant) bytes of adress
+  
+  Wire.endTransmission(); // Send device adress, memmory adress and a stop bit
+
+  Wire.requestFrom(SL_AD, (uint8_t)4);    // Wait for 4 bytes from the device (NTP5332)
+
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read();   // Read bytes as char
+    Serial.print(c, HEX);      // Print the character
+    Serial.print(", ");
+  }
+  Serial.println("");
 }
 
 
@@ -63,56 +82,22 @@ void setup() {
 
 
 void loop() {
-  
+
+delay(5000);
+NTP_EEPROM_read(NTP_ADDRESS, 0x00, 0x00);
+NTP_EEPROM_write(NTP_ADDRESS, 0x00, 0x00, 0x00,0x00,0x00,0x00);
+NTP_EEPROM_read(NTP_ADDRESS, 0x00, 0x00);
+delay(5000);
+NTP_EEPROM_write(NTP_ADDRESS, 0x00, 0x00, 0x01,0x02,0x03,0x04);
+
+
+  /*
  //"Software interrupt"
   if(!digitalRead(ED_PIN)){ // If filed is presented
     ED_ISR();
   } 
+  */
 
-  //NTP_write(NTP_ADDRESS, 0x00, 0x02, 4, 0x01, 0x02, 0x03, 0x04);
-
-  delay(10);
-
-
-
-  Serial.println("Writing data (AA) to adress 00 02");
-  
-  //WRITE DATA p. 102
-  Wire.beginTransmission(NTP_ADDRESS); 
-  
-  Wire.write(0x00); // sends first (most significant) bytes of adress
-  Wire.write(0x02); // sends second (least significant) bytes of adress
-  
-  // Sends data to be stored
-  // 4 Data blocks should be sent for writing to EEPROM
-  Wire.write(0xAB); //DATA 0
-  Wire.write(0xBA); //DATA 1
-  Wire.write(0xFA); //DATA 2
-  Wire.write(0xBA); //DATA 3 
-
-  Wire.endTransmission();    // stop transmitting
-
-  delay(500);
-
-
-  Serial.println("Reading data from adress 00 02");
-  //READ DATA p. 102
-  //Prepare adress
-  Wire.beginTransmission(NTP_ADDRESS); // Write start adress 
-
-  Wire.write(0x00); // sends first (most significant) bytes of adress
-  Wire.write(0x02); // sends second (least significant) bytes of adress
-
-  Wire.endTransmission(); // Send device adress, memmory adress and a stop bit
-
-  Wire.requestFrom(NTP_ADDRESS, 16);    // Wait for 16 bytes from the device (NTP5332)
-
-  while (Wire.available()) { // slave may send less than requested
-    char c = Wire.read();   // Read bytes as char
-    Serial.print(c);      // Print the character
-    Serial.print(", ");
-  }
-  Serial.println("Done");
 
 
 }
